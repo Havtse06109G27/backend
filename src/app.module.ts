d57791e6dbe170a489c8a7e672d8d8aebdf8e4c1
havtse06109 <havtse06109@gmail.com>
2023-11-29 23:19:59 +0700

    feat(api): update upload image api and search image api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,3 @@
-/* eslint-disable prettier/prettier */
-// import { Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { ImagesModule } from './images/images.module';
-
-// @Module({
-//   imports: [MongooseModule.forRoot('mongodb://localhost:27017'), ImagesModule],
-//   controllers: [],
-//   providers: [],
-// })
-// export class AppModule { }
-
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ImageModule } from './images/images.module';
